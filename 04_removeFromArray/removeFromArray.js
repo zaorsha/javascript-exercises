4c6ca92ca4048c943fb4531076bb5679b411394d
zaorsha <zaorsha@gmail.com>
2023-09-01 23:50:36 +0100

    WorkingOn: 04_removeFromArray
    test 1 passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(testArray, item) {
+    let newArray = [];
+    for (let i in testArray) {
+        if (testArray[i] == item) {
+            continue;
+        } else {
+            newArray.push(testArray[i]);
+        }
+    } return newArray;
 };
 
 // Do not edit below this line
