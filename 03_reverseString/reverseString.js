b05da822098083cd9557967052dd33b19957c9f3
zaorsha <zaorsha@gmail.com>
2023-09-01 23:27:09 +0100

    Complete: 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,6 +2,7 @@ const reverseString = function(string) {
     const charArray = string.split("");
     const reversedArray = charArray.reverse();
     let newString = reversedArray.join("");
+    return newString;
 };
 
 // Do not edit below this line
