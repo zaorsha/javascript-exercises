b8ea4bd363ec6845afab8f0dda1ec1d77da290a1
zaorsha <zaorsha@gmail.com>
2023-09-01 23:18:48 +0100

    03_reverseString not working
    trying to use an array to reverse the
    string and then join back to a string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const charArray = string.split("");
+    const reversedArray = charArray.reverse();
+    let newString = reversedArray.join("");
 };
 
 // Do not edit below this line
