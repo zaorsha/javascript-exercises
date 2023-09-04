6f68520526477b02a8214585a4b238622bf7fda9
zaorsha <zaorsha@gmail.com>
2023-09-04 23:01:57 +0100

    Complete: 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,31 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let smallNum;
+    let largeNum;
 
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    if (typeof num1 != 'number' || typeof num2 != 'number') {
+        return "ERROR";
+    }
+
+    if (num1 > num2){
+        smallNum = num2;
+        largeNum = num1;
+    } else {
+        smallNum = num1;
+        largeNum = num2;
+    }
+
+    let i = smallNum;
+    let sum = smallNum;
+
+    while (i < largeNum) {
+        i += 1;
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
