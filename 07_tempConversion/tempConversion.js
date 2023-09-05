646f08fc8474f2aacfb63caa3fc65a2de1ea0006
zaorsha <zaorsha@gmail.com>
2023-09-05 23:49:18 +0100

    Complete: 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  num = num - 32;
+  num = num * 5;
+  num = num / 9;
+  return Math.round(num * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  num = num * 9;
+  num = num / 5;
+  num = num + 32;
+  return Math.round(num * 10) / 10;
 };
 
 // Do not edit below this line
