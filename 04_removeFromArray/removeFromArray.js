f7b5788c819e0b688a05c1a25cc4628a2c24dc49
zaorsha <zaorsha@gmail.com>
2025-08-21 22:59:43 +0100

    js exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,15 @@
 const removeFromArray = function(testArray, ...item) {
+
     let newArray = [];
-    for (let i in testArray) {
-        if (item.includes(testArray[i])) {
-            continue;
-        } else {
+
+    for (let i = 0; i < testArray.length; i++) {
+        if (item.includes(testArray[i]) == false) {
             newArray.push(testArray[i]);
         }
-    } return newArray;
+    }
+
+    return newArray;
+
 };
 
 // Do not edit below this line
